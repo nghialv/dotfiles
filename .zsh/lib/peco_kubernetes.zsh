e196be93c28642e8726b293bfaba55e9eeb7a4fa
nghialv <nghialv2607@gmail.com>
2017-07-27 23:11:28 +0900

    update kube peco

--- .zsh/lib/peco_kubernetes.zsh
+++ .zsh/lib/peco_kubernetes.zsh
@@ -1,17 +1,15 @@
-# Get pod
-function peco-kubernetes-get-pod {
+# Pick pod
+function kpickpod {
   local pod=$(kubectl get pods | peco --prompt "[pods]" | awk '{print $1}')
-
   BUFFER="${BUFFER}${pod}"
   CURSOR=$#BUFFER
   zle redisplay
 }
-zle -N peco-kubernetes-get-pod
-bindkey '^p' peco-kubernetes-get-pod
+zle -N kpickpod
+bindkey '^p' kpickpod
 
 # Port forward
-
-function peco-kubernetes-port-forward {
+function kforward {
   local port="$1"
 
   if [ -z "$port" ] ; then
@@ -28,5 +26,24 @@ function peco-kubernetes-port-forward {
   echo "kubectl port-forward ${pod} ${ports}"
   kubectl port-forward $pod $ports
 }
-alias kpf="peco-kubernetes-port-forward"
 
+# Logs
+function klogs {
+  local selected=$(kubectl get deployments,statefulset --no-headers | cut -d ' ' -f 1 | peco --query "$LBUFFER" | cut -d '/' -f 2)
+  if [ -n "$selected" ]; then
+    print -s "stern ${selected}" # register history
+    stern ${selected}
+  fi
+}
+
+# Context
+function kctx() {
+  local selected_context=$(kubectl config view -o go-template --template='{{range .contexts}}{{.name}}{{"\n"}}{{end}}' | peco --query "$LBUFFER")
+  if [ -n "$selected_context" ]; then
+    BUFFER="kubectl config use-context $selected_context"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N kctx
+bindkey '^K' kctx
